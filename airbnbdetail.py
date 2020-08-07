ebc6aa3954306697cad685b5783d08c2da1a5c48
yoonwooseong <57824259+yoonwooseong@users.noreply.github.com>
2020-08-07 23:45:45 +0900

    revise function extract_detail
    revise function extract_detail

--- airbnbdetail.py
+++ airbnbdetail.py
@@ -6,20 +6,14 @@ URL = "https://www.airbnb.co.kr/rooms/"
 
 def extract_detail(accommodation_idxs):
     for room_idx in accommodation_idxs:
-        print(room_idx)
         room = room_idx
-        result = requests.get(f"{URL}"+room)
-        soup = BeautifulSoup(result.text, "html.parser")
-
-        results = soup.find("div",{"class","_tqmy57"})
-        if results is not None:
-            print(results)
-        else:
-            while True:
-                result = requests.get(f"{URL}"+room)
-                soup = BeautifulSoup(result.text, "html.parser")
-                if results is not None:
-                    print(results)
-                    break;
-                else:
-                    print("try again")
+        
+        while True:
+            result = requests.get(f"{URL}"+room)
+            soup = BeautifulSoup(result.text, "html.parser")
+            results = soup.find("div",{"class","_tqmy57"})
+            if results is not None:
+                print(results)
+                break;
+            else:
+                print("try again")
