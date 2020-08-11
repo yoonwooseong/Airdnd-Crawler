59a26f38950db8e89111d32f03b50c4509141e38
yoonwooseong <57824259+yoonwooseong@users.noreply.github.com>
2020-08-11 17:47:13 +0900

    add section
    add section

--- airbnbdetail.py
+++ airbnbdetail.py
@@ -1,20 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.airbnb.co.kr/rooms/"
+URL_BASE = "https://www.airbnb.co.kr/rooms/"
+URL_PARAM = "?check_in=2020-08-01&check_out=2020-08-03"
 
 def extract_detail(accommodation_idxs):
     for room_idx in accommodation_idxs:
         room = room_idx
+        URL = URL_BASE+room+URL_PARAM
 
         while True:
-            result = requests.get(f"{URL}"+room)
+            result = requests.get(f"{URL}")
             soup = BeautifulSoup(result.text, "html.parser")
             results = soup.find("div",{"class","_tqmy57"})
+            # room_pictures = soup.find("div", {"class","_1h6n1zu"})
+
             if results is not None:
+
                 room_name = soup.find("div", {"class","_mbmcsn"}).find("h1").get_text(strip=True)
                 room_scores = soup.find("span", {"class","_1jpdmc0"})
 
+                # None일때 오류 방지
                 if room_scores is not None:
                     room_score = room_scores.get_text(strip=True)
                 else:
@@ -29,11 +35,22 @@ def extract_detail(accommodation_idxs):
                 room_types , room_options = results.find_all("div", recursive=False)
                 room_type = room_types.get_text(strip=True)
                 room_option = room_options.get_text(strip=True)
+                room_rules_sort = soup.find_all("div", {"class","_1qsawv5"})
+                room_rules_content = soup.find_all("div", {"class","_1jlr81g"})
+                room_price = soup.find("span",{"class","_pgfqnw"})
+                # room_picture = room_pictures.find("picture")
+
+                # print부분은 나중에 함수로 따로 빼기 !!
                 print()
+                print(URL)
                 print(room_name)
                 print(room_score, room_review_num)
                 print(room_type)
                 print(room_option)
+                print(room_rules_sort)
+                print(room_rules_content)
+                print(f"{room_price}/박")
+                # print(room_picture)
                 print()
                 break;
             else:
