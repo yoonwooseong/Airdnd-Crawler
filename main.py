d1fd54e3581739fda77b367880b25239a4ad389b
yoonwooseong <57824259+yoonwooseong@users.noreply.github.com>
2020-08-25 14:56:04 +0900

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from airbnbsearchlist import get_accommodation_infos
-from airbnbdetail import extract_detail
-
-accommodation_infos = get_accommodation_infos() 
- 
-extract_detail(accommodation_infos)
